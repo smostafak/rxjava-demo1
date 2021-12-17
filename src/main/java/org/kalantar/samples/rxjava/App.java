005a6f7353aa2b5ff89e05bf4c93fd037e98a8d4
Kalantar <kalantar@caspco.ir>
2021-12-17 18:33:02 +0330

    Using Observable.just()

--- src/main/java/org/kalantar/samples/rxjava/App.java
+++ src/main/java/org/kalantar/samples/rxjava/App.java
@@ -4,20 +4,9 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class App {
   public static void main(String[] args) {
-    final Observable<String> src = Observable.create(emitter -> {
-      try {
-        emitter.onNext("Alpha");
-        emitter.onNext("Beta");
-        emitter.onNext("Gamma");
-        emitter.onComplete();
-      } catch (Throwable e) {
-        emitter.onError(e);
-      }
-    });
-
-    src.map(String::length)
+    Observable.just("Alpha", "Beta", "Gamma")
+        .map(String::length)
         .filter(i -> i >= 5)
-        .subscribe(o ->
-            System.out.println("Received: " + o), Throwable::printStackTrace);
+        .subscribe(i -> System.out.println("RCV: " + i));
   }
 }
