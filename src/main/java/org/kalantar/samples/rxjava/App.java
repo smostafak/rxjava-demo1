9d85d4309cd31ad8b2b8f7f9a5d73573e9de1f9c
Kalantar <kalantar@caspco.ir>
2021-12-18 22:19:18 +0330

    Pass three lambdas as arguments to subscribe() method

--- src/main/java/org/kalantar/samples/rxjava/App.java
+++ src/main/java/org/kalantar/samples/rxjava/App.java
@@ -1,33 +1,15 @@
 package org.kalantar.samples.rxjava;
 
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.disposables.Disposable;
 
 public class App {
   public static void main(String[] args) {
     Observable.just("Alpha", "Beta", "Gamma")
         .map(String::length)
         .filter(i -> i >= 5)
-        .subscribe(new Observer<>() {
-          @Override
-          public void onSubscribe(@NonNull Disposable d) {}
-
-          @Override
-          public void onNext(@NonNull Integer value) {
-            System.out.println("RECEIVED: " + value);
-          }
-
-          @Override
-          public void onError(@NonNull Throwable e) {
-            e.printStackTrace();
-          }
-
-          @Override
-          public void onComplete() {
-            System.out.println("Done!");
-          }
-        });
+        .subscribe(
+            i -> System.out.println("RECEIVED: " + i),
+            Throwable::printStackTrace,
+            () -> System.out.println("Done!"));
   }
 }
