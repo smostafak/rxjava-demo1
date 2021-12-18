66e73b42861ff9979209d17f7f2072c61baa89cf
Kalantar <kalantar@caspco.ir>
2021-12-18 22:14:01 +0330

    Use subscribe with Observer impl.

--- src/main/java/org/kalantar/samples/rxjava/App.java
+++ src/main/java/org/kalantar/samples/rxjava/App.java
@@ -1,12 +1,33 @@
 package org.kalantar.samples.rxjava;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public class App {
   public static void main(String[] args) {
     Observable.just("Alpha", "Beta", "Gamma")
         .map(String::length)
         .filter(i -> i >= 5)
-        .subscribe(i -> System.out.println("RCV: " + i));
+        .subscribe(new Observer<>() {
+          @Override
+          public void onSubscribe(@NonNull Disposable d) {}
+
+          @Override
+          public void onNext(@NonNull Integer value) {
+            System.out.println("RECEIVED: " + value);
+          }
+
+          @Override
+          public void onError(@NonNull Throwable e) {
+            e.printStackTrace();
+          }
+
+          @Override
+          public void onComplete() {
+            System.out.println("Done!");
+          }
+        });
   }
 }
