e966096896177d84337cd71fdd2287fadd9d89f9
kalantar <msalzmg1>
2021-12-19 13:02:56 +0330

    The second Observer transforms its emissions with operators, but it will still get its own stream of emissions!

--- src/main/java/org/kalantar/samples/rxjava/App.java
+++ src/main/java/org/kalantar/samples/rxjava/App.java
@@ -3,9 +3,11 @@ package org.kalantar.samples.rxjava;
 import io.reactivex.rxjava3.core.Observable;
 
 public class App {
-  public static void main(String[] args) {
-    final Observable<String> src = Observable.just("Alpha", "Beta", "Gamma");
-    src.subscribe(s -> System.out.println("Observer 1: " + s));
-    src.subscribe(s -> System.out.println("Observer 2: " + s));
-  }
+    public static void main(String[] args) {
+        final Observable<String> src = Observable.just("Alpha", "Beta", "Gamma");
+        src.subscribe(s -> System.out.println("Observer 1: " + s));
+        src.map(String::length)
+                .filter(i -> i >= 5)
+                .subscribe(s -> System.out.println("Observer 2: " + s));
+    }
 }
