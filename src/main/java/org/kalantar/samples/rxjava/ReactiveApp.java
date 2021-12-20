6c0c976263af28064530e880ad881992544f5d6b
kalantar <msalzmg1>
2021-12-20 15:04:38 +0330

    Creates an Observable that emits a consecutive range of integers using Observable.range()

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -2,15 +2,10 @@ package org.kalantar.samples.rxjava;
 
 
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.observables.ConnectableObservable;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        final ConnectableObservable<String> src =
-                Observable.just("Alpha", "Beta", "Gamma").publish();
-        src.subscribe(s -> System.out.println("[" + Thread.currentThread().getId() + "] Observer 1: " + s));
-        src.map(String::length)
-                .subscribe(s -> System.out.println("[" + Thread.currentThread().getId() + "] Observer 2: " + s));
-        src.connect();
+        Observable.range(1, 13)
+                .subscribe(i -> System.out.println("RCV: " + i));
     }
 }
