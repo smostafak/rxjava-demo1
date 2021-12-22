2734fe2f3bb8f5ade3e6300566605ba69e4822b8
kalantar <msalzmg1>
2021-12-22 15:00:28 +0330

    Observable to Single

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -1,12 +1,13 @@
 package org.kalantar.samples.rxjava;
 
 
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        Single.just("Hello!")
-                .map(String::length)
-                .subscribe(System.out::println, e -> System.out.println("ERR: " + e));
+        final Single<String> first = Observable.just("Alpha", "Beta")
+                .first("NULL");
+        first.subscribe(System.out::println);
     }
 }
