7951b5e8b308239796350740b2db82ae1b4b6dd7
kalantar <msalzmg1>
2021-12-22 14:41:35 +0330

    Observable was not even created, so we do not see the ERR message.

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -4,14 +4,9 @@ package org.kalantar.samples.rxjava;
 import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveApp {
-    private static int START = 1;
-    private static int COUNT = 3;
-
     public static void main(String[] args) {
-        final var range = Observable.defer(() ->  Observable.range(START, COUNT));
-        final var name = Thread.currentThread().getName();
-        range.subscribe(i -> System.out.println("[" + name + "] Observer 1: " + i));
-        COUNT = 5;
-        range.subscribe(i -> System.out.println("[" + name + "] Observer 2: " + i));
+        Observable.just(1 / 0)
+                .subscribe(i -> System.out.println("RCV: " + i),
+                        e -> System.out.println("ERR: " + e));
     }
 }
