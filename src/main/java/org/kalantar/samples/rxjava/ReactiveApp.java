b30127734d428fb37be77af8a80fc669dcdafac4
kalantar <msalzmg1>
2021-12-22 15:11:09 +0330

    The message Process 1 done! does not come up because there is no ambiguity: the Maybe observable cannot emit more than one item, so it is completed implicitly

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -1,13 +1,18 @@
 package org.kalantar.samples.rxjava;
 
 
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Single;
+import io.reactivex.Maybe;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        final Single<String> first = Observable.just("Alpha", "Beta")
-                .first("NULL");
-        first.subscribe(System.out::println);
+        Maybe.just(100)
+                .subscribe(integer -> System.out.println("Process 1: " + integer),
+                        e -> System.out.println("ERR: " + e),
+                        () -> System.out.println("Process 1 Done!"));
+
+        Maybe.empty()
+                .subscribe(integer -> System.out.println("Process 2: " + integer),
+                        e -> System.out.println("ERR: " + e),
+                        () -> System.out.println("Process 2 Done!"));
     }
 }
