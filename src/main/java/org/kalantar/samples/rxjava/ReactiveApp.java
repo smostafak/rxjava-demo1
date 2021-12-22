82a015e0d1ca5aecc6d08977ac9194668dedc374
kalantar <msalzmg1>
2021-12-22 15:56:14 +0330

    Stop receiving emissions from Observable.interval() after 5 seconds using Disposable::dispose()

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -1,12 +1,20 @@
 package org.kalantar.samples.rxjava;
 
 
-import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+
+import java.util.concurrent.TimeUnit;
 
 public class ReactiveApp {
-    public static void main(String[] args) {
-        Completable.fromRunnable(() -> System.out.println("Process"))
-                .subscribe(() -> System.out.println("Done!"));
+    public static void main(String[] args) throws InterruptedException {
+        final Disposable disposable = Observable.interval(1, TimeUnit.SECONDS)
+                .subscribe(i -> System.out.println("RCV: " + i));
 
+        Thread.currentThread().join(5000);
+
+        disposable.dispose();
+
+        Thread.currentThread().join(5000);
     }
 }
