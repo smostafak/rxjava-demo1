da93f408cc86027c84cd211ded764c981359f4a9
kalantar <msalzmg1>
2021-12-22 14:47:06 +0330

    Emit the exception down the chain along with an onError event, even if thrown during the emission initialization using Observable.fromCallable()

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -5,8 +5,7 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        Observable.just(1 )
-                .map(i -> i / 0)
+        Observable.fromCallable(() -> 1 / 0)
                 .subscribe(i -> System.out.println("RCV: " + i),
                         e -> System.out.println("ERR: " + e));
     }
