f623ab9d193aff183f407d44e27913b6ea98928b
kalantar <msalzmg1>
2021-12-22 14:44:41 +0330

    The exception is occurred after the Observable is created, so we can see the ERR message.

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -5,7 +5,8 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        Observable.just(1 / 0)
+        Observable.just(1 )
+                .map(i -> i / 0)
                 .subscribe(i -> System.out.println("RCV: " + i),
                         e -> System.out.println("ERR: " + e));
     }
