bf81dc095015c0fd299d022046590a3cdfe7841c
kalantar <msalzmg1>
2021-12-22 14:30:18 +0330

    Observable.range() does not make sense from when COUNT change

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -4,8 +4,14 @@ package org.kalantar.samples.rxjava;
 import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveApp {
+    private static int START = 1;
+    private static int COUNT = 3;
+
     public static void main(String[] args) {
-        Observable.range(1, 13)
-                .subscribe(i -> System.out.println("RCV: " + i));
+        final var range = Observable.range(START, COUNT);
+        final var name = Thread.currentThread().getName();
+        range.subscribe(i -> System.out.println("[" + name + "] Observer 1: " + i));
+        COUNT = 5;
+        range.subscribe(i -> System.out.println("[" + name + "] Observer 2: " + i));
     }
 }
