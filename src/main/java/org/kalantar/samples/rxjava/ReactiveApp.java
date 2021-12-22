3ac4c50390024d597dceba67dc50b0a3ef496234
kalantar <msalzmg1>
2021-12-22 15:18:22 +0330

    firstElement() operator yield a Maybe

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -1,19 +1,17 @@
 package org.kalantar.samples.rxjava;
 
 
-import io.reactivex.Maybe;
+import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        Observable.just(100)
-                .subscribe(integer -> System.out.println("Process 1: " + integer),
-                        e -> System.out.println("ERR: " + e),
-                        () -> System.out.println("Process 1 Done!"));
+        final Maybe<String> maybe = Observable.just("Alpha", "Beta")
+                .firstElement();
+        maybe.subscribe(i -> System.out.println("RCV: " + i),
+                e -> System.out.println("ERR: " + e),
+                () -> System.out.println("Done!"));
 
-        Observable.empty()
-                .subscribe(integer -> System.out.println("Process 2: " + integer),
-                        e -> System.out.println("ERR: " + e),
-                        () -> System.out.println("Process 2 Done!"));
+
     }
 }
