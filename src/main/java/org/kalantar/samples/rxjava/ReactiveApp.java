72563982cdbf82759be37dcacf7c13a80a3b14cb
kalantar <msalzmg1>
2021-12-22 15:12:02 +0330

    Now, the onComplete event was issued to tell the Observer that it should not expect anything else

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -2,15 +2,16 @@ package org.kalantar.samples.rxjava;
 
 
 import io.reactivex.Maybe;
+import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveApp {
     public static void main(String[] args) {
-        Maybe.just(100)
+        Observable.just(100)
                 .subscribe(integer -> System.out.println("Process 1: " + integer),
                         e -> System.out.println("ERR: " + e),
                         () -> System.out.println("Process 1 Done!"));
 
-        Maybe.empty()
+        Observable.empty()
                 .subscribe(integer -> System.out.println("Process 2: " + integer),
                         e -> System.out.println("ERR: " + e),
                         () -> System.out.println("Process 2 Done!"));
