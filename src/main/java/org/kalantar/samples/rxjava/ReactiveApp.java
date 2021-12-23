b42b3edbfbc147cc4a2bd70dc05e380c62e87704
Kalantar <kalantar@caspco.ir>
2021-12-23 19:51:43 +0330

    By default, calling subscribe() with an Observer instance don’t return a Disposable, but it is possible to get it by extending ResourceObserver and subscribing using subscribeWith()

--- src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
+++ src/main/java/org/kalantar/samples/rxjava/ReactiveApp.java
@@ -2,19 +2,39 @@ package org.kalantar.samples.rxjava;
 
 
 import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.ResourceObserver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ReactiveApp {
-    public static void main(String[] args) throws InterruptedException {
-        final Disposable disposable = Observable.interval(1, TimeUnit.SECONDS)
-                .subscribe(i -> System.out.println("RCV: " + i));
+  public static void main(String[] args) throws InterruptedException {
+    final Disposable disposable = Observable.interval(1, TimeUnit.SECONDS)
+        .subscribeWith(new ResourceObserver<>() {
 
-        Thread.currentThread().join(5000);
+          @Override
+          public void onNext(@NonNull Long aLong) {
+            System.out.println(aLong);
+          }
 
-        disposable.dispose();
+          @Override
+          public void onError(@NonNull Throwable e) {
+            e.printStackTrace();
+          }
 
-        Thread.currentThread().join(5000);
-    }
+          @Override
+          public void onComplete() {
+            System.out.println("Done!");
+          }
+        });
+
+    Thread.currentThread().join(5000);
+
+    disposable.dispose();
+
+    System.out.println("Wait 5s...");
+
+    Thread.currentThread().join(5000);
+  }
 }
